6aff5cec5bf8aeedc52e1cf0986abe982b4f345e
abhi15-bose-max <abhinavbasu156@gmail.com>
2026-07-29 17:09:56 +0530

    Update sc_multiplier.v

--- src/compute/sc_multiplier.v
+++ src/compute/sc_multiplier.v
@@ -1,1 +1,30 @@
+/*
+------------------------------------------------------------------------------
+File        : sc_multiplier.v
 
+Description :
+Stochastic multiplier.
+
+Performs stochastic multiplication using a single AND gate.
+
+Inputs:
+    a_bit
+    b_bit
+
+Output:
+    product_bit
+
+------------------------------------------------------------------------------
+*/
+
+module sc_multiplier
+(
+    input  wire a_bit,
+    input  wire b_bit,
+
+    output wire product_bit
+);
+
+assign product_bit = a_bit & b_bit;
+
+endmodule
