5457b170d56a7cf68f86cbb5102c7e4c068da054
abhi15-bose-max <abhinavbasu156@gmail.com>
2026-07-29 18:24:39 +0530

    Update negative_accumulator.v

--- src/compute/negative_accumulator.v
+++ src/compute/negative_accumulator.v
@@ -1,1 +1,35 @@
+/*
+------------------------------------------------------------------------------
+File        : negative_accumulator.v
 
+Description :
+Accumulates the negative popcount values over the entire stochastic stream.
+
+------------------------------------------------------------------------------
+*/
+
+`include "config.vh"
+
+module negative_accumulator
+(
+    input  wire                         clk,
+    input  wire                         rst,
+    input  wire                         enable,
+
+    input  wire [`POPCOUNT_WIDTH-1:0]   count,
+
+    output reg  [`ACC_WIDTH-1:0]        accumulated_sum
+);
+
+always @(posedge clk)
+begin
+
+    if (rst)
+        accumulated_sum <= 0;
+
+    else if (enable)
+        accumulated_sum <= accumulated_sum + count;
+
+end
+
+endmodule
