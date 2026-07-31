e4fd6e60f67415a6e7f3d6fa4b469297979eb292
abhi15-bose-max <abhinavbasu156@gmail.com>
2026-07-31 10:38:02 +0530

    Update pnr_compute_core.v

--- synthesis/pnr_compute_core.v
+++ synthesis/pnr_compute_core.v
@@ -1,59 +1,59 @@
 `timescale 1ns / 1ps
 `include "config.vh"
 
-module pnr_compute_core (
-
+module pnr_compute_core
+(
     input  wire clk,
     input  wire reset,
 
     output wire debug
-
 );
 
     //------------------------------------------------------------
-    // Internal stimulus registers
+    // Internal stimulus
     //------------------------------------------------------------
 
-    reg [`NUM_STREAMS-1:0] operand_a;
-    reg [`NUM_STREAMS-1:0] operand_b;
+    reg [`DOT_PRODUCT_SIZE-1:0] operand_a = 0;
+    reg [`DOT_PRODUCT_SIZE-1:0] operand_b = 1;
 
-    reg [`NUM_STREAMS-1:0] sign_a;
-    reg [`NUM_STREAMS-1:0] sign_b;
+    reg [`DOT_PRODUCT_SIZE-1:0] sign_a = 0;
+    reg [`DOT_PRODUCT_SIZE-1:0] sign_b = 0;
 
-    wire signed [`ACC_WIDTH-1:0] result;
+    wire signed [`ACC_WIDTH:0] result;
 
     //------------------------------------------------------------
-    // Simple stimulus generation
+    // Generate activity
     //------------------------------------------------------------
 
-    always @(posedge clk) begin
-
-        if (reset) begin
-
+    always @(posedge clk)
+    begin
+        if (reset)
+        begin
             operand_a <= 0;
             operand_b <= 1;
 
             sign_a <= 0;
-            sign_b <= {`NUM_STREAMS{1'b1}};
-
+            sign_b <= 0;
         end
-        else begin
-
+        else
+        begin
             operand_a <= operand_a + 1'b1;
             operand_b <= operand_b + 2'b10;
 
             sign_a <= operand_a;
             sign_b <= operand_b;
-
         end
-
     end
 
     //------------------------------------------------------------
     // Compute Core
     //------------------------------------------------------------
 
-    synth_compute_core compute_core (
+    synth_compute_core compute_core
+    (
+        .clk(clk),
+        .rst(reset),
+        .enable(1'b1),
 
         .operand_a(operand_a),
         .operand_b(operand_b),
@@ -61,11 +61,7 @@ module pnr_compute_core (
         .sign_a(sign_a),
         .sign_b(sign_b),
 
-        .clk(clk),
-        .reset(reset),
-
         .result(result)
-
     );
 
     //------------------------------------------------------------
